12346aa3e68c51d2ac212d587bdaabb6dcf30e50
zjxscdd123 <1305941575@qq.com>
2021-12-12 17:32:06 +0800

    proj1b

---
+++ proj1b/Palindrome.java
@@ -0,0 +1,29 @@
+public class Palindrome {
+    public Deque<Character> wordToDeque(String word) {
+        LinkedListDeque<Character> dq = new LinkedListDeque<>();
+        for (int i = 0;i < word.length();i++) {
+            dq.addLast(word.charAt(i));
+        }
+        return dq;
+    }
+
+    public boolean isPalindrome(String word) {
+        Deque<Character> dq = wordToDeque(word);
+        while (dq.size() > 1) {
+            if (dq.removeFirst() != dq.removeLast()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(String word, CharacterComparator cc) {
+        Deque<Character> dq = wordToDeque(word);
+        while (dq.size() > 1) {
+            if (!cc.equalChars(dq.removeFirst(), dq.removeLast())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
